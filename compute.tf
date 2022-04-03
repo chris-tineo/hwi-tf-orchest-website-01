30457085723a8b328d2f248ddb33c3b675da76dc
chris-tineo <52720074+chris-tineo@users.noreply.github.com>
2022-04-02 23:45:08 -0500

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -5,3 +5,13 @@ module "resourcegroup" {
   rsrc_name     = "RGWEBSITE01PROD"
   rsrc_location = "East US 2" 
 }
+
+module "appservice" {
+  source  = "app.terraform.io/HelloWorldInc/appservice/azurerm"
+  version = "0.0.1"
+  
+  rsrc_location = "aps"
+  rsrc_name     = "apswebsprd01"
+  rsrc_rg       =  module.resourcegroup.name
+  rsrc_sku      =  "F1"
+}
