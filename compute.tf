9055665d14a4d19df0c34a609813b65924a28118
chris-tineo <52720074+chris-tineo@users.noreply.github.com>
2022-04-06 04:09:08 -0500

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -15,3 +15,32 @@ module "staticsite" {
   rsrc_rg         =  module.resourcegroup.rg_name
   rsrc_skugeneral = "Standard"
 }
+  
+#######Resources needed to Alert Static Site using Azure Monitor
+  
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "stacwebsprd01"
+  resource_group_name      = module.resourcegroup.rg_name
+  location                 = "East US 2"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_service_plan" "serviceplan" {
+  name                = "srplwebsprd01"
+  resource_group_name = module.resourcegroup.rg_name
+  location            = "East US 2"
+  os_type             = "Linux"
+  sku_name            = "S1"
+}
+
+resource "azurerm_linux_function_app" "lnx_function_app" {
+  name                = "lnfnwebsprd01"
+  resource_group_name = module.resourcegroup.rg_name
+  location            = "East US 2"
+
+  storage_account_name = azurerm_storage_account.storage_account.name
+  service_plan_id      = azurerm_service_plan.serviceplan.id
+
+  site_config {}
+}
