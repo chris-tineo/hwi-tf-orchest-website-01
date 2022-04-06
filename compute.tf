887ee148717dc1f6bbe9a3d3925dab5994b82f34
chris-tineo <52720074+chris-tineo@users.noreply.github.com>
2022-04-06 04:34:35 -0500

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -33,3 +33,61 @@ resource "azurerm_application_insights" "appinsights" {
   workspace_id        = azurerm_log_analytics_workspace.loganalytics.id
   application_type    = "web"
 }
+  
+resource "azurerm_application_insights_web_test" "example" {
+  name                    = "webtest-static-web-prod"
+  location                = "East US 2"
+  resource_group_name     = module.resourcegroup.rg_name
+  application_insights_id = azurerm_application_insights.appinsights.id
+  kind                    = "ping"
+  frequency               = 300
+  timeout                 = 60
+  enabled                 = true
+  geo_locations           = ["us-tx-sn1-azr", "us-il-ch1-azr"]
+
+  configuration = <<XML
+<WebTest Name="WebTest1" Id="ABD48585-0831-40CB-9069-682EA6BB3583" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="0" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
+  <Items>
+    <Request Method="GET" Guid="a5f10126-e4cd-570d-961c-cea43999a200" Version="1.1" Url="http://microsoft.com" ThinkTime="0" Timeout="300" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
+  </Items>
+</WebTest>
+XML
+
+}  
+/*
+resource "azurerm_monitor_metric_alert" "example" {
+  name                = "Alert Static Web App Prod"
+  resource_group_name = module.resourcegroup.rg_name
+  scopes              = [azurerm_storage_account.to_monitor.id]
+  description         = "Action will be triggered when Transactions count is greater than 50."
+
+  criteria {
+    metric_namespace = "Microsoft.Storage/storageAccounts"
+    metric_name      = "Transactions"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 50
+
+    dimension {
+      name     = "ApiName"
+      operator = "Include"
+      values   = ["*"]
+    }
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.main.id
+  }
+}  
+
+resource "azurerm_monitor_action_group" "main" {
+  name                = "example-actiongroup"
+  resource_group_name = azurerm_resource_group.main.name
+  short_name          = "exampleact"
+
+  webhook_receiver {
+    name        = "callmyapi"
+    service_uri = "http://example.com/alert"
+  }
+}
+ */
