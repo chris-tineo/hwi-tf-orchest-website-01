dddffe9a7ffb39600b7e1b0a69e51e28ecb8b51a
chris-tineo <52720074+chris-tineo@users.noreply.github.com>
2022-04-06 02:20:03 -0500

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,29 @@
+resource "azurerm_key_vault" "keyvault01" {
+  name                        = "keyvwebsprd01"
+  location                    = "East US 2"
+  resource_group_name         = module.resourcegroup.rg_name
+  enabled_for_disk_encryption = false
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+/*
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = 
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+    */
+}
